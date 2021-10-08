69d4fb12a3d81c62b26e2106bfdfdebbe450232c
jbtrystram <jbtrystram@redhat.com>
2021-10-08 11:58:58 +0200

    import files

---
+++ files/src/devices.rs
@@ -0,0 +1,292 @@
+use crate::config::Context;
+use crate::{util, AppId, DeviceId, Verbs};
+use anyhow::{anyhow, Context as AnyhowContext, Result};
+use json_value_merge::Merge;
+use oauth2::TokenResponse;
+use reqwest::blocking::Client;
+use reqwest::blocking::Response;
+use reqwest::{StatusCode, Url};
+use serde_json::{from_str, json, Value};
+use std::process::exit;
+use tabular::{Row, Table};
+
+fn craft_url(base: &Url, app_id: &str, device_id: Option<&str>) -> String {
+    let device = match device_id {
+        Some(dev) => format!("/{}", dev),
+        None => String::new(),
+    };
+    format!(
+        "{}{}/apps/{}/devices{}",
+        base,
+        util::REGISTRY_API_PATH,
+        app_id,
+        device
+    )
+}
+
+pub fn delete(
+    config: &Context,
+    app: AppId,
+    device_id: DeviceId,
+    ignore_missing: bool,
+) -> Result<()> {
+    let client = Client::new();
+    let url = craft_url(&config.registry_url, &app, Some(&device_id));
+
+    client
+        .delete(&url)
+        .bearer_auth(&config.token.access_token().secret())
+        .send()
+        .context("Can't delete device.")
+        .map(|res| {
+            if ignore_missing && res.status() == StatusCode::NOT_FOUND {
+                exit(0);
+            } else {
+                util::print_result(res, format!("Device {}", device_id), Verbs::delete)
+            }
+        })
+}
+
+pub fn read(config: &Context, app: AppId, device_id: DeviceId) -> Result<()> {
+    get(&config, &app, &device_id)
+        .map(|res| util::print_result(res, device_id.to_string(), Verbs::get))
+}
+
+pub fn create(
+    config: &Context,
+    device_id: DeviceId,
+    data: serde_json::Value,
+    app_id: AppId,
+    file: Option<&str>,
+) -> Result<()> {
+    let data = if data == json!({}) {
+        json!({"credentials": {}})
+    } else {
+        data
+    };
+
+    let body = match file {
+        Some(f) => util::get_data_from_file(f)?,
+        None => {
+            json!({
+            "metadata": {
+                "name": device_id,
+                "application": app_id
+            },
+            "spec": data
+            })
+        }
+    };
+
+    let client = Client::new();
+    let url = craft_url(&config.registry_url, &app_id, None);
+
+    client
+        .post(&url)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .bearer_auth(&config.token.access_token().secret())
+        .body(body.to_string())
+        .send()
+        .context("Can't create device.")
+        .map(|res| util::print_result(res, format!("Device {}", device_id), Verbs::create))
+}
+
+pub fn edit(config: &Context, app: AppId, device_id: DeviceId, file: Option<&str>) -> Result<()> {
+    match file {
+        Some(f) => {
+            let data = util::get_data_from_file(f)?;
+
+            put(&config, &app, &device_id, data)
+                .map(|res| util::print_result(res, format!("Device {}", device_id), Verbs::edit))
+        }
+        None => {
+            //read device data
+            let res = get(&config, &app, &device_id);
+            match res {
+                Ok(r) => match r.status() {
+                    StatusCode::OK => {
+                        let body = r.text().unwrap_or_else(|_| "{}".to_string());
+                        let insert = util::editor(body)?;
+                        put(&config, &app, &device_id, insert).map(|p| {
+                            util::print_result(p, format!("Device {}", device_id), Verbs::edit)
+                        })
+                    }
+                    e => {
+                        log::error!("Error : could not retrieve device: {}", e);
+                        util::exit_with_code(e)
+                    }
+                },
+                Err(e) => {
+                    log::error!("Error : could not execute request: {}", e);
+                    exit(2)
+                }
+            }
+        }
+    }
+}
+
+
+Hello !
+
+pub fn list(config: &Context, app: AppId, labels: Option<String>) -> Result<()> {
+    let client = Client::new();
+    let url = craft_url(&config.registry_url, &app, None);
+
+    let mut req = client
+        .get(&url)
+        .bearer_auth(&config.token.access_token().secret());
+
+    if let Some(labels) = labels {
+        req = req.query(&[("labels", labels)]);
+    }
+
+    let res = req.send().context("Can't list devices");
+
+    if let Ok(r) = res {
+        if r.status() == StatusCode::OK {
+            pretty_list(r.text()?)?;
+            Ok(())
+        } else {
+            Err(anyhow!("List operation failed with {}", r.status()))
+        }
+    } else {
+        Err(anyhow!("Error while requesting devices list."))
+    }
+}
+
+pub fn set_gateway(
+    config: &Context,
+    app: AppId,
+    device_id: DeviceId,
+    gateway_id: DeviceId,
+) -> Result<()> {
+    // prepare json data to merge
+    let data = json!({"spec": {
+    "gatewaySelector": {
+        "matchNames": [gateway_id]
+    }
+    }});
+
+    set(config, app, device_id, data)
+}
+
+pub fn set_password(
+    config: &Context,
+    app: AppId,
+    device_id: DeviceId,
+    password: String,
+    username: Option<&str>,
+) -> Result<()> {
+    let credential = match username {
+        Some(user) => json!({"user": {"username": user, "password": password}}),
+        None => json!({ "pass": password }),
+    };
+
+    // prepare json data to merge
+    let data = json!({"spec": {
+    "credentials": {
+        "credentials": [
+          credential
+        ]
+    }
+    }});
+
+    set(config, app, device_id, data)
+}
+
+pub fn add_alias(
+    config: &Context,
+    app: AppId,
+    device_id: DeviceId,
+    new_alias: String,
+) -> Result<()> {
+    // prepare json data to merge
+    let data = json!({"spec": {
+    "alias": [
+          new_alias
+        ]
+    }});
+
+    set(config, app, device_id, data)
+}
+
+// The "set" operation merges the data with what already exists on the server side
+fn set(config: &Context, app: AppId, device_id: DeviceId, data: Value) -> Result<()> {
+    //read device data
+    let res = get(&config, &app, &device_id);
+    match res {
+        Ok(r) => match r.status() {
+            StatusCode::OK => {
+                let mut body: Value =
+                    serde_json::from_str(r.text().unwrap_or_else(|_| "{}".to_string()).as_str())?;
+                body.merge(data);
+                put(&config, &app, &device_id, body)
+                    .map(|p| util::print_result(p, format!("Device {}", device_id), Verbs::edit))
+            }
+            e => {
+                log::error!("Error : could not retrieve device: {}", e);
+                util::exit_with_code(e)
+            }
+        },
+        Err(e) => {
+            log::error!("Error : could not execute request: {}", e);
+            exit(2)
+        }
+    }
+}
+
+fn get(config: &Context, app: &str, device_id: &DeviceId) -> Result<Response> {
+    let client = Client::new();
+    let url = craft_url(&config.registry_url, app, Some(&device_id));
+
+    client
+        .get(&url)
+        .bearer_auth(&config.token.access_token().secret())
+        .send()
+        .context("Can't get device.")
+}
+
+fn put(
+    config: &Context,
+    app: &AppId,
+    device_id: &DeviceId,
+    data: serde_json::Value,
+) -> Result<Response> {
+    let client = Client::new();
+    let url = craft_url(&config.registry_url, app, Some(&device_id));
+    let token = &config.token.access_token().secret();
+
+    client
+        .put(&url)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .bearer_auth(token)
+        .body(data.to_string())
+        .send()
+        .context(format!(
+            "Error while updating device data for {}",
+            device_id
+        ))
+}
+
+// todo drogue-client and the types would be useful for this
+fn pretty_list(data: String) -> Result<()> {
+    let device_array: Vec<Value> = from_str(data.as_str())?;
+
+    let mut table = Table::new("{:<} {:<}");
+    table.add_row(Row::new().with_cell("NAME").with_cell("AGE"));
+
+    for dev in device_array {
+        let name = dev["metadata"]["name"].as_str();
+        let creation = dev["metadata"]["creationTimestamp"].as_str();
+        if let Some(name) = name {
+            table.add_row(
+                Row::new()
+                    .with_cell(name)
+                    .with_cell(util::age(creation.unwrap())?),
+            );
+        }
+    }
+
+    print!("{}", table);
+    Ok(())
+}
---
+++ files/src/openid.rs
@@ -0,0 +1,178 @@
+use oauth2::basic::{BasicClient, BasicTokenResponse};
+use oauth2::reqwest::http_client;
+use oauth2::{
+    AuthUrl, AuthorizationCode, ClientId, CsrfToken, PkceCodeChallenge, RedirectUrl, Scope,
+    TokenResponse, TokenUrl,
+};
+
+use anyhow::Error;
+use anyhow::Result;
+
+use tiny_http::{Response, Server};
+
+use qstring::QString;
+use reqwest::Url;
+
+use crate::config::{self, Context};
+use crate::util;
+use chrono::{DateTime, Duration, Utc};
+use std::net::{Ipv4Addr, SocketAddr};
+
+const CLIENT_ID: &str = "drogue";
+
+
+
+fn get_token(auth_url: Url, token_url: Url) -> Result<BasicTokenResponse> {
+    log::debug!("Using auth url : {}", auth_url);
+
+    //start a local server
+    let bind = SocketAddr::from((Ipv4Addr::LOCALHOST, 0));
+    let server = Server::http(bind).unwrap();
+    let port = server.server_addr().port();
+
+    let client = BasicClient::new(
+        ClientId::new(CLIENT_ID.to_string()),
+        None,
+        AuthUrl::new(auth_url.to_string())?,
+        Some(TokenUrl::new(token_url.to_string())?),
+    )
+    // Where the user will be redirected to after the authorization process.
+    .set_redirect_url(RedirectUrl::new(format!("http://localhost:{}", port))?);
+
+    // Generate a PKCE challenge. As this is a client app a PKCE challenge this is needed to assure confidentiality.
+    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    // Generate the full authorization URL.
+    let (final_auth_url, csrf_token) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("offline_access".to_string()))
+        .set_pkce_challenge(pkce_challenge)
+        .url();
+
+    // The URL the user should browse to, in order to trigger the authorization process.
+    log::info!("Opening browser.");
+    if webbrowser::open(final_auth_url.as_str()).is_err() {
+        log::warn!("Failed to open browser.");
+        println!(
+            "\nTo authenticate with drogue cloud please browse to: \n{}",
+            final_auth_url
+        );
+    }
+
+    // get the request from the localhost webserver
+    let request = server.recv()?;
+
+    // extract code and state from the openID server request
+    let querry = QString::from(request.url().trim_start_matches("/?"));
+    let state = querry.get("state").unwrap();
+    let code = querry.get("code").unwrap();
+
+    log::info!("Authentication code retrieved.");
+    log::debug!("Trading auth code with token using url : {}", token_url);
+
+    // For security reasons, verify that the `state` parameter returned by the server matches `csrf_state`.
+    assert_eq!(csrf_token.secret().as_str(), state);
+
+    // Now trade it for an access token.
+    let token_result = client
+        .exchange_code(AuthorizationCode::new(code.to_string()))
+        // Set the PKCE code verifier.
+        .set_pkce_verifier(pkce_verifier)
+        .request(http_client);
+
+    let browser_msg = match token_result {
+        Ok(_) => "Authentication success. This browser can be closed.",
+        Err(_) => "Authentication failed! This browser can be closed.",
+    };
+    let _ = request.respond(Response::from_string(browser_msg));
+
+    // Unwrapping token_result will either produce a Token or a RequestTokenError.
+    token_result.map_err(|e| {
+        Error::msg(format!(
+            "error while requesting a token: \n{}",
+            e.to_string()
+        ))
+    })
+}
+
+pub fn verify_token_validity(context: &mut Context) -> Result<bool> {
+    log::debug!("Token expires at : {}", context.token_exp_date);
+    // 30 seconds should be enough
+    if context.token_exp_date - Utc::now() > Duration::seconds(30) {
+        Ok(false)
+    } else {
+        log::info!("Token is expired or will be soon, refreshing...");
+        refresh_token(context)
+    }
+}
+
+fn refresh_token(context: &mut Context) -> Result<bool> {
+    let refresh_token_var = context
+        .token
+        .refresh_token()
+        .ok_or_else(|| Error::msg("Error loading refresh token from config"))?;
+    let new_token = exchange_token(
+        context.auth_url.clone(),
+        context.token_url.clone(),
+        &refresh_token_var,
+    )?;
+
+    context.token_exp_date = calculate_token_expiration_date(&new_token)?;
+    context.token = new_token;
+
+    log::info!("New token will expire at {}", context.token_exp_date);
+    log::info!("Token successfully refreshed.");
+
+    Ok(true)
+}
+
+fn exchange_token(
+    auth_url: Url,
+    token_url: Url,
+    refresh_token_val: &oauth2::RefreshToken,
+) -> Result<BasicTokenResponse> {
+    log::debug!("Refreshing token using url : {}", &token_url);
+
+    let auth_url = AuthUrl::new(auth_url.to_string())?;
+    let token_url = TokenUrl::new(token_url.to_string())?;
+
+    let client = BasicClient::new(
+        ClientId::new(CLIENT_ID.to_string()),
+        None,
+        auth_url,
+        Some(token_url),
+    );
+
+    // Exchange the refresh token for access token
+    client
+        .exchange_refresh_token(refresh_token_val)
+        .request(http_client)
+        .map_err(|e| {
+            log::warn!("{:?}", e);
+            Error::msg(format!("While refreshing token : {}", e))
+        })
+}
+
+fn calculate_token_expiration_date(token: &BasicTokenResponse) -> Result<DateTime<Utc>> {
+    let now = Utc::now();
+    let expiration = token
+        .expires_in()
+        .ok_or_else(|| anyhow::Error::msg("Missing expiration time on token"))?;
+
+    now.checked_add_signed(Duration::from_std(expiration)?)
+        .ok_or_else(|| anyhow::Error::msg("Error calculating token expiration date"))
+}
+
+pub fn print_token(context: &Context) {
+    println!("{}", context.token.access_token().secret());
+}
+pub fn print_whoami(context: &Context) {
+    println!("Cluster adress : {}", context.drogue_cloud_url);
+    println!(
+        "Default App : {}",
+        context
+            .default_app
+            .as_ref()
+            .unwrap_or(&"No default app".to_string())
+    );
+}
